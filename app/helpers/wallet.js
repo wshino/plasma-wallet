20b645b9bfcc5d2b4f7cc7a4bd326ac1ecc38e78
SHUHEI HIYA <syuhei176@gmail.com>
2019-02-05 19:29:26 +0900

    Use layer2/wallet module

--- app/helpers/wallet.js
+++ app/helpers/wallet.js
@@ -1,107 +1,31 @@
 import Web3 from 'web3';
 import {
-  Transaction,
-  TransactionOutput
-} from '@cryptoeconomicslab/chamber-core';
+  ChamberWallet,
+  PlasmaClient
+} from '@layer2/wallet';
 import {
-  BaseWallet
-} from '@cryptoeconomicslab/chamber-wallet';
-import ChildChainApi from '../helpers/childchain';
-import {
-  BigStorage,
-  Storage
+  WalletStorage
 } from './storage';
-import utils from 'ethereumjs-util';
-
-const WALLET_MODE_UNKNOWM = 0;
-const WALLET_MODE_METAMASK = 1;
-const WALLET_MODE_MOBILE = 2;
+import {
+  JsonRpcClient
+} from './jsonrpc'
 
 /**
  * Plasma wallet store UTXO and proof
  */
-export default class PlasmaWallet extends BaseWallet {
+export default class WalletFactory {
 
-  constructor() {
-    super({
-      storage: Storage,
-      bigStorage: new BigStorage()
-    });
-    this.childChainApi = new ChildChainApi(process.env.CHILDCHAIN_ENDPOINT || 'http://localhost:3000');
-    // what we have
-    this.utxos = Storage.load('utxo') || {};
-    this.latestBlockNumber = 0;
-    this.loadedBlockNumber = Storage.load('loadedBlockNumber') || 0;
-    // privKey is Buffer
-    this.privKey = null;
-    this.zeroHash = utils.sha3(0).toString('hex');
-    this.mode = WALLET_MODE_UNKNOWM;
-  }
-
-  getAddress() {
-    return this.address;
-  }
-
-  initWeb3() {
-    const web3 = window.web3;
-    const privateKeyHex = Storage.load('privateKey') || 'c87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3';
-    this.privKey = new Buffer(privateKeyHex, 'hex');
-    if (typeof web3 !== 'undefined') {
-      // MetaMask
-      this.mode = WALLET_MODE_METAMASK;
-      const web3tmp = new Web3(web3.currentProvider);
-      this.web3 = web3tmp;
-      this.web3Child = web3tmp;
-      return this.web3.eth.getAccounts().then(accounts => {
-        this.setAddress(accounts[0])
-        return Promise.resolve();
-      });
-    }else{
-      // Mobile
-      this.mode = WALLET_MODE_MOBILE;
-      const web3 = new Web3(new Web3.providers.HttpProvider(
-        process.env.CHILDCHAIN_ENDPOINT || 'http://localhost:3000'
-      ));
-      const web3Root = new Web3(new Web3.providers.HttpProvider(
-        process.env.ROOTCHAIN_ENDPOINT || 'http://localhost:8545'
-      ));
-      const address = utils.privateToAddress(this.privKey);
-      web3Root.eth.defaultAccount = utils.bufferToHex(address);
-      web3Root.eth.accounts.wallet.add(utils.bufferToHex(this.privKey));
-      this.web3 = web3Root;
-      this.web3Child = web3;
-      this.setAddress(utils.toChecksumAddress(utils.bufferToHex(address)))
-      return Promise.resolve();
-    }
-  }
-
-  getHistory(utxoKey) {
-    return this.bigStorage.searchProof(utxoKey);
-  }
-  
-  /**
-   * @dev sign transaction by private key
-   * @param {Transaction} tx
-   */
-  async sign(tx) {
-    if(this.mode == WALLET_MODE_METAMASK) {
-      const accounts = await this.web3.eth.getAccounts();
-      return await this.web3.eth.sign(utils.bufferToHex(tx.hash()), accounts[0]);
-    }else{
-      return tx.sign(this.privKey);
-    }
-  }
-
-  /**
-   * @dev generate key from UTXO
-   * @param {TransactionOutput} data 
-   */
-  static getUTXOKey(data) {
-    if(data.owners && data.value && data.state && data.hasOwnProperty('blkNum')) {
-      return utils.sha3(JSON.stringify(data)).toString('hex');
-    }else{
-      throw new Error('invalid UTXO');
-    }
+  static createWallet() {
+    const jsonRpcClient = new JsonRpcClient(process.env.CHILDCHAIN_ENDPOINT || 'http://localhost:3000')
+    const client = new PlasmaClient(jsonRpcClient)
+    const storage = new WalletStorage()
+    return new ChamberWallet(
+      client,
+      '0xc87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3',
+      'http://127.0.0.1:8545',
+      '0x30753e4a8aad7f8597332e813735def5dd395028',
+      storage
+    )
   }
 
 }
